2fab0230f6e9e8c5971f00793b04d8514d5f94e8
alex840623 <tommy840623@gmail.com>
2023-03-29 19:54:57 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const URL = require('./models/url')
 const app = express()
+
+// 加入這段 code, 僅在非正式環境時, 使用 dotenv
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
 
 // 設定首頁路由
 app.get('/', (req, res) => {
