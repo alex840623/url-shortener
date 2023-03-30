ae69bedd825edbaa213f8cda97a9ed21fafafe7d
alex840623 <tommy840623@gmail.com>
2023-03-30 17:22:01 +0800

    feat: add main function

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const exphbs = require('express-handlebars')
 const URL = require('./models/url')
+const generateUrl = require('./generate_url')
 const app = express()
 
 // 加入這段 code, 僅在非正式環境時, 使用 dotenv
@@ -25,10 +26,27 @@ db.once('open', () => {
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+// 產生新網址畫面
+app.post('/url', (req, res) => {
+  const url = req.body.url
+  if (!url) return res.redirect('/') //網址為空倒回首頁
+  const urlHeader = req.headers.host
+  const randomNum = generateUrl(url)
+
+  URL.findOne({ url: url })
+    .then(data => data ? data : URL.create({ url, newUrl: randomNum })
+    )   //若網址不存在則製造短網址並存入資料庫
+    .then(data => res.render('show', { newUrl: data.newUrl, url, urlHeader })
+    )   //若網址存在則會載入原本短網址
+    .catch(error => console.error(error))
+})  
 
 // 設定 port 3000
 app.listen(3000, () => {
